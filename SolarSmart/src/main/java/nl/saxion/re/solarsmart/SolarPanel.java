b1aa4c43858d99957ffbf9f76e397986807721a5
IvoHristoff <hristovivo24@gmail.com>
2023-04-08 02:06:31 +0200

    added calculations for roofArea, fitting panels on roofArea, powerConsumption per year with working buttons for 3phaseConnection and shadeRoof

--- SolarSmart/src/main/java/nl/saxion/re/solarsmart/SolarPanel.java
+++ SolarSmart/src/main/java/nl/saxion/re/solarsmart/SolarPanel.java
@@ -2,21 +2,29 @@ package nl.saxion.re.solarsmart;
 
 public class SolarPanel extends Item{
 
-    private double length;
+    private double height;
     private double width;
     private double generatedPower;
 
-    public SolarPanel (String name, double price, double length, double width, double generatedPower) {
+    public SolarPanel () {
+
+        super("SunBurn Factor 50", 200);
+        this.height = 1.754;
+        this.width = 1.096;
+        this.generatedPower = 405;
+
+    }
+    /* overloading https://www.mygreatlearning.com/blog/method-overloading-in-java/#:~:text=Method%20overloading%20in%20java%20is,name%2C%20but%20with%20different%20parameters. */
+    public SolarPanel (String name, double price, double height, double width, double generatedPower) {
 
         super(name, price);
-        this.length = length;
+        this.height = height;
         this.width = width;
         this.generatedPower = generatedPower;
 
     }
-
     public double getLength() {
-        return length;
+        return height;
     }
 
     public double getWidth() {
--- SolarSmart/src/main/java/nl/saxion/re/solarsmart/SolarSmartController.java
+++ SolarSmart/src/main/java/nl/saxion/re/solarsmart/SolarSmartController.java
@@ -3,24 +3,16 @@ package nl.saxion.re.solarsmart;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
-import java.io.PrintStream;
-import javafx.application.Application;
-import javafx.geometry.Insets;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
-import java.io.PrintStream;
 import java.text.DecimalFormat;
-
 import javafx.scene.control.TextArea;
+import static java.lang.Math.floor;
 
 
 public class SolarSmartController {
 
-    // --"T"-- stands for TextField //
+    /* --"T"-- stands for TextField */
     @FXML
     public TextField firstNameT;
     @FXML
@@ -32,6 +24,10 @@ public class SolarSmartController {
     @FXML
     public TextField powerConsT;
     @FXML
+    public ToggleGroup toggleShade = new ToggleGroup();
+    @FXML
+    public ToggleGroup togglePhase = new ToggleGroup();
+    @FXML
     public RadioButton onshadeNo = new RadioButton();
     @FXML
     public RadioButton onshadeYes = new RadioButton();
@@ -42,24 +38,24 @@ public class SolarSmartController {
 
     @FXML
     public TextArea calculations;
-    // ------------------------------------ //
+    /* ------------------------------------ */
     public TextArea textArea;
     public String firstName;
     public String lastName;
     public double roofW;
     public double roofH;
     public int powerCons;
-    public boolean shadeRoofYes = true;
-    public boolean shadeRoofNo = false;
+    public boolean shadeRoof = true;
     public boolean phaseConnector = false;
 
     public void onConfirm(ActionEvent event) {
+        double panelW = 1.754;
+        double panelH = 1.096;
+        int panelWatts = 405;
 
         firstName = firstNameT.getText();
-        textArea.appendText("Mr/Ms " + firstName + "\n");
-
         lastName = lastNameT.getText();
-        textArea.appendText(lastName + "\n");
+        textArea.appendText("Mr/Ms " + firstName + " " + lastName + "\n");
 
         roofW = Double.parseDouble(roofWT.getText());
         textArea.appendText(roofW + "m Width" + "\n");
@@ -70,15 +66,44 @@ public class SolarSmartController {
         powerCons = Integer.parseInt(powerConsT.getText());
         textArea.appendText(powerCons + " Watts" + "\n");
 
-        // ----- formatting the double's numbers after the comma to the second one ---- //
+        /* ----- formatting the double's numbers after the comma to the second one ---- */
         double roofArea = roofH*roofW;
         DecimalFormat df = new DecimalFormat("#.##");
         String formattedroofArea = df.format(roofArea);
 
-        // -----this code prints in the textarea---- //
-        // !!! (IT DOES SHOW ONLY AFTER THE FIRST TIME THE CONFIRM BUTTON IS CLICKED) !!! //
+        /* -----this code prints in the textarea---- */
+        /* !!! (IT DOES SHOW ONLY AFTER THE FIRST TIME THE CONFIRM BUTTON IS CLICKED) !!! */
+        int numberOfPanels = (int) (floor(roofW/panelW)*floor(roofH/panelH));
+        /* for some reason shadeRoof is switched, but it works */
+        int kwhPerYear = (int) (numberOfPanels * panelWatts * (this.shadeRoof? 0.8: 0.5));
+        /* if button is clicked, then add 3m additional or whatever, else */
         textArea.appendText("The total roof area is: "+ formattedroofArea + " m2." + "\n");
+        textArea.appendText("Number of panels to fit: "+ numberOfPanels + "\n");
+        textArea.appendText("Power output of the panels: "+ kwhPerYear);
         textArea = new TextArea();
         textArea.setEditable(false);
+
+    }
+    public void onShadeToggle() {
+        /*/ Create the RadioButtons and add them to the ToggleGroup */
+        this.onshadeNo.setToggleGroup(this.toggleShade);
+        this.onshadeYes.setToggleGroup(this.toggleShade);
+
+        /* Check which RadioButton is selected */
+        RadioButton selectedRadioButton = (RadioButton) this.toggleShade.getSelectedToggle();
+        String selectedText = selectedRadioButton.getText();
+        this.shadeRoof = selectedText.equals("yes");
+    }
+
+    public void onPhaseToggle() {
+        /* Create the RadioButtons and add them to the ToggleGroup */
+        this.onphaseNo.setToggleGroup(this.togglePhase);
+        this.onphaseYes.setToggleGroup(this.togglePhase);
+
+        /* Check which RadioButton is selected */
+        RadioButton selectedRadioButton = (RadioButton) this.togglePhase.getSelectedToggle();
+        String selectedText = selectedRadioButton.getText();
+        this.phaseConnector = selectedText.equals("yes");
+
     }
 }
